970e650a383ee3be11424f5e3b19443a0a700607
katie424 <7080katie@gmail.com>
2025-02-06 01:14:58 +0900

    ui: 레이아웃 설정

--- frontend/src/pages/StudentIDE.jsx
+++ frontend/src/pages/StudentIDE.jsx
@@ -1,5 +1,26 @@
+import HalfScreen from "../components/HalfScreen";
+import { useNavigate } from "react-router-dom";
+
 const StudentIDE = () => {
-  return <div>StudentIDE</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div className="flex flex-col h-10 items-start justify-center bg-primary text-white border-b border-white">
+        <span
+          className="flex items-center gap-4 py-2 px-5 cursor-pointer"
+          onClick={() => navigate(`/classroom`)}
+        >
+          <span>{"<"}</span>
+          <span>강의실로 돌아가기</span>
+        </span>
+      </div>
+      <div className="grid grid-cols-2 w-full h-full">
+        <HalfScreen title="Teacher"></HalfScreen>
+        <HalfScreen title="Student"></HalfScreen>
+      </div>
+    </div>
+  );
 };
 
 export default StudentIDE;
--- frontend/src/pages/TeacherIDE.jsx
+++ frontend/src/pages/TeacherIDE.jsx
@@ -1,5 +1,25 @@
+import HalfScreen from "../components/HalfScreen";
+import { useNavigate } from "react-router-dom";
+
 const TeacherIDE = () => {
-  return <div>TeacherIDE</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div className="flex flex-col h-10 items-start justify-center bg-primary text-white border-b border-white">
+        <span
+          className="flex items-center gap-4 py-2 px-5 cursor-pointer"
+          onClick={() => navigate(`/classroom`)}
+        >
+          <span>{"<"}</span>
+          <span>강의실로 돌아가기</span>
+        </span>
+      </div>
+      <div className="grid grid-cols-[3fr_1fr] w-full h-full">
+        <HalfScreen title="Teacher">{/* 코드 작성 영역 */}</HalfScreen>
+      </div>
+    </div>
+  );
 };
 
 export default TeacherIDE;
