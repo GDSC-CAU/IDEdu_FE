3f06a94c2205b06b1666f62702e733d8375a3d4f
katie424 <7080katie@gmail.com>
2025-03-04 23:54:32 +0900

    design: 수업 상태 추가

--- frontend/src/pages/TeacherIDE.jsx
+++ frontend/src/pages/TeacherIDE.jsx
@@ -19,11 +19,14 @@ const TeacherIDE = () => {
         <HalfScreen title="Teacher">
           <CodeEditor />
         </HalfScreen>
-        <div className="flex flex-col items-center p-5 gap-5">
+        <div className="flex flex-col items-center p-5 gap-3">
           <button className="bg-secondary p-2 w-4/5 hover:bg-primary hover:text-white">
             수업 시작하기
           </button>
-          <div className="flex flex-row justify-between items-center w-full">
+          <div className="text-center text-sm text-gray-400">
+            수업 시작 전입니다. <br /> 화면을 공유하려면 수업을 시작하세요.
+          </div>
+          <div className="flex flex-row justify-between items-center w-full mt-2">
             <h1 className="text-lg">학생명단</h1>
             <span className="mr-4">도움</span>
           </div>
