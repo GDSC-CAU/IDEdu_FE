e2549738fdd17119ecb92dc60957a5f191c5d5db
katie424 <7080katie@gmail.com>
2025-03-13 19:50:56 +0900

    feat: 코드에디터 키값 추가

--- frontend/src/pages/StudentIDE.jsx
+++ frontend/src/pages/StudentIDE.jsx
@@ -1,17 +1,22 @@
 import HalfScreen from "../components/HalfScreen";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import CodeEditor from "../components/CodeEditor";
 import goback from "../assets/goback-w.png";
+import { useClassroom } from "../hooks/useClassroomData";
+import { useState } from "react";
 
 const StudentIDE = () => {
   const navigate = useNavigate();
+  const { courseId } = useParams();
+  const { teacherIdeId, studentIdeId } = useClassroom(courseId);
+  const [userId] = useState(() => parseInt(Math.random() * 1000000)); // ✅ 한 번 생성 후 유지
 
   return (
     <div className="flex flex-col h-screen w-screen">
       <div className="flex flex-col h-10 items-start justify-center bg-primary text-white border-b border-white">
         <span
           className="flex items-center gap-4 py-2 px-5 pr-16 cursor-pointer"
-          onClick={() => navigate(`/classroom`)}
+          onClick={() => navigate(`/classroom/${courseId}`)}
         >
           <img src={goback} alt="goback" className="w-4" />
           <span>강의실로 돌아가기</span>
@@ -19,10 +24,18 @@ const StudentIDE = () => {
       </div>
       <div className="grid grid-cols-2 w-full h-full">
         <HalfScreen title="Teacher">
-          수업이 아직 시작되지 않았습니다.
+          <CodeEditor
+            key={`${userId}-${teacherIdeId}`}
+            ideId={teacherIdeId}
+            userId={userId}
+          />
         </HalfScreen>
         <HalfScreen title="Student">
-          <CodeEditor />
+          <CodeEditor
+            key={`${userId}-${studentIdeId}`}
+            ideId={studentIdeId}
+            userId={userId}
+          />
         </HalfScreen>
       </div>
     </div>
